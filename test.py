bba0955d489674183b61a4211dcb36e59c02f3c7
cjhsu1991 <dd86042@gmail.com>
2017-11-29 00:10:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import jaydebeapi
-server="10.63.7.6"
-database="cp_inventory"
-user="cp_inv"
-password="taisweet"
+server="ip位置"
+database="資料庫名稱"
+user="資料庫帳號"
+password="資料庫密碼"
 
 jclassname='com.sybase.jdbc3.jdbc.SybDriver'
-url='jdbc:sybase:Tds:10.63.7.6:5000/cp_inventory'
+url='jdbc:sybase:Tds:IP位置:5000/資料庫名稱'
 driver_args=[url,user,password]
-jars="/home/jconn3.jar"
+jars="/home/jconn3.jar"  #放置JAR的路徑要記得加到CLASSPATH    
 conn=jaydebeapi.connect(jclassname,driver_args,jars)
 
 curs = conn.cursor()
@@ -17,7 +17,4 @@ curs.execute("select * from TEMPREP_MA124")
 row = curs.fetchall()
 
 while row is not None:
- 
   print(row)
- 
-  row = cursor.fetchone()
